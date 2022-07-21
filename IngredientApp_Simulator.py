23d5fe7be7bfe68a99756bc3564a875f70007d24
DanaY-1 <yargesd@oregonstate.edu>
2022-07-20 21:40:07 -0700

    Calculations and example tests added

--- IngredientApp_Simulator.py
+++ IngredientApp_Simulator.py
@@ -26,13 +26,77 @@ s_name = s_name.decode()                # store name of microservice for ease of
 print("Type EXIT to quit")
 conn.send(name.encode())                # send app name
 
-# example message
-example_message = {
+#######################################################################################################
+# example messages
+#
+#
+#######################################################################################################
+example_message1 = {
     "amount": 1,
     "from_units": "tbsp",
     "to_units": "tsp"
 }
 
+example_message2 = {
+    "amount": 4,
+    "from_units": "tbsp",
+    "to_units": "tsp"
+}
+
+example_message3 = {
+    "amount": 9,
+    "from_units": "tsp",
+    "to_units": "tbsp"
+}
+
+example_message4 = {
+    "amount": 7,
+    "from_units": "tsp",
+    "to_units": "tbsp"
+}
+
+example_message5 = {
+    "amount": 5,
+    "from_units": "tsp",
+    "to_units": "tbsp"
+}
+
+example_message6 = {
+    "amount": 35,
+    "from_units": "tsp",
+    "to_units": "cups"
+}
+
+example_message7 = {
+    "amount": 35,
+    "from_units": "tsp",
+    "to_units": "floz"
+}
+
+example_message8 = {
+    "amount": 4,
+    "from_units": "cups",
+    "to_units": "floz"
+}
+
+example_message9 = {
+    "amount": 4,
+    "from_units": "floz",
+    "to_units": "tsp"
+}
+
+example_message10 = {
+    "amount": 4,
+    "from_units": "tsp",
+    "to_units": "tsp"
+}
+
+
+#######################################################################################################
+# Communication Pipeline
+#
+#
+#######################################################################################################
 # Continuous Communication Pipeline
 while True:
     message = input(str(name+": "))
@@ -40,8 +104,35 @@ while True:
         conn.send(message.encode())
         print("Exiting...\n")
         break
-    elif message == "example":
-        serialized_dict = json.dumps(example_message)
+    elif message == "example1":
+        serialized_dict = json.dumps(example_message1)
+        message = serialized_dict
+    elif message == "example2":
+        serialized_dict = json.dumps(example_message2)
+        message = serialized_dict
+    elif message == "example3":
+        serialized_dict = json.dumps(example_message3)
+        message = serialized_dict
+    elif message == "example4":
+        serialized_dict = json.dumps(example_message4)
+        message = serialized_dict
+    elif message == "example5":
+        serialized_dict = json.dumps(example_message5)
+        message = serialized_dict
+    elif message == "example6":
+        serialized_dict = json.dumps(example_message6)
+        message = serialized_dict
+    elif message == "example7":
+        serialized_dict = json.dumps(example_message7)
+        message = serialized_dict
+    elif message == "example8":
+        serialized_dict = json.dumps(example_message8)
+        message = serialized_dict
+    elif message == "example9":
+        serialized_dict = json.dumps(example_message9)
+        message = serialized_dict
+    elif message == "example10":
+        serialized_dict = json.dumps(example_message10)
         message = serialized_dict
     conn.send(message.encode())         # send app message
     message = conn.recv(1024)           # receive message from microservice
--- microservice_MeasurementConverter.py
+++ microservice_MeasurementConverter.py
@@ -27,20 +27,142 @@ s_name = s_name.decode()                        # store name of app for ease of 
 print("Type EXIT to quit")
 print("Enter message to send...")
 
+#######################################################################################################
 # Measurement Conversions
+#
+#
+#######################################################################################################
 def tbsp2tsp(msg):
+    """Convert tablespoons to teaspoons
+    """
     try:
         tbsp_val = float(msg["amount"])
     except:
         return "Error"
-    tsp = tbsp_val * 3
+    tsp = round(tbsp_val * 3, 1)
     return tsp
 
+def tsp2tbsp(msg):
+    """Convert teaspoons to tablespoons
+    """
+    try:
+        tsp_val = float(msg["amount"])
+    except:
+        return "Error"
+    tbsp = round(tsp_val / 3, 1)
+    return tbsp
+
+def tbsp2cups(msg):
+    """Convert tablespoons to cups
+    """
+    try:
+        tbsp_val = float(msg["amount"])
+    except:
+        return "Error"
+    cups = round(tbsp_val * 0.0625, 1)
+    return cups
+
+def cups2tbsp(msg):
+    """Convert cups to tablespoons
+    """
+    try:
+        cups_val = float(msg["amount"])
+    except:
+        return "Error"
+    tbsp = round(cups_val * 16, 1)
+    return tbsp
+
+def tbsp2floz(msg):
+    """Convert tablespoons to fluid ounces
+    """
+    try:
+        tbsp_val = float(msg["amount"])
+    except:
+        return "Error"
+    floz = round(tbsp_val / 2, 1)
+    return floz
+
+def floz2tbsp(msg):
+    """Convert fluid ounces to tablespoons
+    """
+    try:
+        floz_val = float(msg["amount"])
+    except:
+        return "Error"
+    tbsp = round(floz_val * 2, 1)
+    return tbsp
+
+def cups2floz(msg):
+    """Convert cups to fluid ounces
+    """
+    try:
+        cups_val = float(msg["amount"])
+    except:
+        return "Error"
+    floz = round(cups_val * 8, 1)
+    return floz
+
+def floz2cups(msg):
+    """Convert fluid ounces to cups
+    """
+    try:
+        floz_val = float(msg["amount"])
+    except:
+        return "Error"
+    cups = round(floz_val / 8, 1)
+    return cups
+
+def tsp2cups(msg):
+    """Convert teaspoons to cups
+    """
+    try:
+        tsp_val = float(msg["amount"])
+    except:
+        return "Error"
+    cups = round(tsp_val / 48, 1)
+    return cups
+
+def cups2tsp(msg):
+    """Convert cups to teaspoons
+    """
+    try:
+        cups_val = float(msg["amount"])
+    except:
+        return "Error"
+    tsp = round(cups_val * 48, 1)
+    return tsp
+
+def tsp2floz(msg):
+    """Convert teaspoons to fluid ounces
+    """
+    try:
+        tsp_val = float(msg["amount"])
+    except:
+        return "Error"
+    floz = round(tsp_val / 6, 1)
+    return floz
+
+def floz2tsp(msg):
+    """Convert fluid ounces to teaspoons
+    """
+    try:
+        floz_val = float(msg["amount"])
+    except:
+        return "Error"
+    tsp = round(floz_val * 6, 1)
+    return tsp
+
+#######################################################################################################
+# Communication Pipeline
+#
+#
+#######################################################################################################
 # Continuous Communication Pipeline
 while True:
     message = s.recv(1024)                      # receive message from app (Ingredients App)
     try:
         message = json.loads(message)           # convert serialized json to dictionary
+        msg_type = "dict"
     except:
         msg_type = "non_dict"
         message = message.decode()
@@ -49,8 +171,56 @@ while True:
         print("Exiting...")
         break
     elif msg_type != "non_dict":
-        if message["from_units"] == "tbsp" and message["to_units"] == "tsp":
+        if message["from_units"] == message["to_units"]:
+            message = str(message["amount"])
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "tbsp" and message["to_units"] == "tsp":
             message = str(tbsp2tsp(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "tsp" and message["to_units"] == "tbsp":
+            message = str(tsp2tbsp(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "tsp" and message["to_units"] == "cups":
+            message = str(tsp2cups(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "tsp" and message["to_units"] == "floz":
+            message = str(tsp2floz(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "tbsp" and message["to_units"] == "cups":
+            message = str(tbsp2cups(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "tbsp" and message["to_units"] == "floz":
+            message = str(tbsp2floz(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "cups" and message["to_units"] == "tsp":
+            message = str(cups2tsp(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "cups" and message["to_units"] == "tbsp":
+            message = str(cups2tbsp(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "cups" and message["to_units"] == "floz":
+            message = str(cups2floz(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "floz" and message["to_units"] == "tsp":
+            message = str(floz2tsp(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "floz" and message["to_units"] == "tbsp":
+            message = str(floz2tbsp(message))
+            s.send(message.encode())
+            print(str(name)+": "+str(message))
+        elif message["from_units"] == "floz" and message["to_units"] == "cups":
+            message = str(floz2cups(message))
             s.send(message.encode())
             print(str(name)+": "+str(message))
     else:
